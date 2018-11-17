fd38403b8e9db55f76ad9817ab087566fa2a4ce7
unknown <roi.vazan@hotmail.com>
2018-11-17 15:32:25 +0200

    first commit

---
+++ TopStocks/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TopStocks.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "How to Get to Hot Stocks";
+
+            return View();
+        }
+
+        public string HelloWorld()
+        {
+            return "Hello world";
+        }
+    }
+}
---
+++ TopStocks/Models/Stock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TopStocks.Models
+{
+    public class Stock
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public int StockCurrentPrice { get; set; }
+        public int WeekHigh { get; set; }
+        public int WeekLow { get; set; }
+        public DateTime NextReportDate { get; set; }
+
+
+        // Entity Framework can't save an array
+        ///public List<string> PhotoList { get; set; }
+
+        //public string Photos
+        //{
+         //   get => (PhotoList == null || PhotoList.Count <= 0 ? "" : string.Join(",", PhotoList));
+         //   set => PhotoList = (string.IsNullOrEmpty(value) ? new List<string>() : value.Split(',').ToList());
+        //}
+    }
+}
