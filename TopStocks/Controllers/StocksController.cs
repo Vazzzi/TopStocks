789ae1ca31a7de40bb8a5d2b26e6b775b388819f
unknown <roi.vazan@hotmail.com>
2018-11-19 18:02:50 +0200

    Changed Shared Layout

--- TopStocks/Controllers/StocksController.cs
+++ TopStocks/Controllers/StocksController.cs
@@ -16,13 +16,12 @@ namespace TopStocks.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
-        // GET: Apartments
+        // GET: Stocks
         [Authorize]
         public ActionResult Index()
         {
-            return View(db.Apartments.Where(ap =>
-                ap.Owner == db.Users.FirstOrDefault<ApplicationUser>(user =>
-                    user.UserName == this.User.Identity.Name)));
+            return View();
+                
         }
 
         // Get: Stocks/Manage
@@ -33,35 +32,9 @@ namespace TopStocks.Controllers
                                                             .Select(g => new { g.Key, Count = g.Count() })
                                                             .OrderByDescending(s => s.Count)
                                                             .ToDictionary(s => s.Key, s => s.Count);
-            return View("Statistics", transactions);
+            return View("Manage", transactions);
         }
 
-        // GET: Apartments/Details/5
-        [Authorize]
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-
-            Apartment apartment = db.Apartments.FirstOrDefault(ap => (ap.ID == id) &&
-                                                                      (ap.Owner.UserName == this.User.Identity.Name));
-
-            if (apartment == null)
-            {
-                return HttpNotFound();
-            }
-
-            return View(apartment);
-        }
-
-        // GET: Apartments/Create
-        [Authorize]
-        public ActionResult Create()
-        {
-            return View();
-        }
 
         // POST: Apartments/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
@@ -86,22 +59,22 @@ namespace TopStocks.Controllers
 
                     if (uploadedFile.HasFile())
                     {
-                        PhotoList.Add(UploadApartmentPhoto(uploadedFile));
+                        PhotoList.Add(UploadStockPhoto(uploadedFile));
                     }
                 }
 
                 apartment.PhotoList = PhotoList;
                 apartment.Owner = db.Users.FirstOrDefault(s => s.UserName == this.User.Identity.Name);
 
-                db.Apartments.Add(apartment);
+                db.Stocks.Add(apartment);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("Manage");
             }
 
             return View(apartment);
         }
 
-        private string UploadApartmentPhoto(HttpPostedFileBase uploadedFile)
+        private string UploadStockPhoto(HttpPostedFileBase uploadedFile)
         {
             string relativePath = "UserPhotos/" + this.User.Identity.Name.Replace("@", "------");
             string absolutePath = AppDomain.CurrentDomain.BaseDirectory + relativePath;
@@ -115,79 +88,8 @@ namespace TopStocks.Controllers
             return relativePath + '/' + filename;
         }
 
-        // GET: Apartments/Edit/5
-        [Authorize]
-        public ActionResult Edit(int? id)
-        {
-            db.Configuration.LazyLoadingEnabled = false;
 
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-
-            Apartment apartment = db.Apartments.FirstOrDefault(ap => (ap.ID == id) &&
-                                                                      (ap.Owner.UserName == this.User.Identity.Name));
-
-            if (apartment == null)
-            {
-                return HttpNotFound();
-            }
-
-            return View(apartment);
-        }
-
-        // POST: Apartments/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include =
-                "ID,Owner,Location,Description,PropertyValue,PhotoList,Photos,Balcony,Size,FloorNumber,NumberOfRooms")]
-            Apartment apartment)
-        {
-            if (ModelState.IsValid)
-            {
-                for (int uploadID = 0; uploadID < Request.Files.Count; uploadID++)
-                {
-                    var uploadedFile = Request.Files[uploadID];
-
-                    if (uploadedFile.HasFile())
-                    {
-                        apartment.PhotoList.Add(UploadApartmentPhoto(uploadedFile));
-                    }
-                }
-
-                // Compare and delete all the photos from the folder
-                CompareAndDeletePhotos(apartment);
-
-                db.Entry(apartment).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(apartment);
-        }
-
-        private void CompareAndDeletePhotos(Apartment apartment)
-        {
-            // Get the current db value
-            string dbApartmentPhotoList =
-                db.Apartments.Where(ap => ap.ID == apartment.ID).Select(ap => ap.Photos).ToList()[0];
-
-            if (!String.IsNullOrEmpty(dbApartmentPhotoList))
-            {
-                foreach (string photo in dbApartmentPhotoList.Split(','))
-                {
-                    string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, photo);
-
-                    if (!apartment.PhotoList.Contains(photo) && System.IO.File.Exists(filePath))
-                    {
-                        System.IO.File.Delete(filePath);
-                    }
-                }
-            }
-        }
+        
 
         // GET: Apartments/Delete/5
         [Authorize]
@@ -209,12 +111,13 @@ namespace TopStocks.Controllers
             return View(apartment);
         }
 
-        // POST: Apartments/Delete/5
+        // POST: Stocks/Delete/5
+        [Authorize(Roles = "Admin")]
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            Apartment apartment = db.Apartments.FirstOrDefault(ap => (ap.ID == id) &&
+            Stock apartment = db.Apartments.FirstOrDefault(ap => (ap.ID == id) &&
                                                                       (ap.Owner.UserName == this.User.Identity.Name));
 
             if (apartment != null)
@@ -275,27 +178,5 @@ namespace TopStocks.Controllers
             return Json(QuerySet, JsonRequestBehavior.AllowGet);
         }
 
-        public bool PredictApartmentSale(int size, int value, int floor)
-        {
-
-            var proc = new Process
-            {
-                StartInfo = new ProcessStartInfo
-                {
-                    FileName = System.AppDomain.CurrentDomain.BaseDirectory + @"..\debug\ML.exe",
-                    Arguments = size.ToString() + " " + value.ToString() + " " + floor.ToString(),
-                    UseShellExecute = false,
-                    RedirectStandardOutput = true,
-                    CreateNoWindow = false
-                }
-            };
-
-            proc.Start();
-            proc.WaitForExit();
-            string output = proc.StandardOutput.ReadToEnd();
-            bool returnBool = output.TrimEnd() == "True";
-
-            return returnBool;
-        }
     }
 }
